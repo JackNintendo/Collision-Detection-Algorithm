db9c3adec4cf8588e21d9a2744cbe723de607670
JackNintendo <71106344+JackNintendo@users.noreply.github.com>
2020-10-22 19:44:59 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "Green";
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "Green";
+}
+
+function draw() {
+  background(255,255,255);  
+
+  //movingRect must be controlled by Mouse
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  //to detect collision
+  if(movingRect.x-fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x-movingRect.x < fixedRect.width/2 + movingRect.width/2
+    && movingRect.y-fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y-movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    fixedRect.shapeColor = "Red";
+    movingRect.shapeColor = "Red";
+  }
+  else{
+    fixedRect.shapeColor = "Green";
+    movingRect.shapeColor = "Green";
+  }
+  drawSprites();
+}
